18bdb3f7f943a9a33f4aa81fabf509e9f4e41d4d
foly13 <foly_13@hotmail.com>
2025-11-14 11:07:51 -0300

    la ultima

--- db.js
+++ db.js
@@ -1,15 +1,18 @@
-// db.js
 import mysql from 'mysql2/promise';
-import 'dotenv/config'; // Carga las variables de entorno
+import 'dotenv/config';
 
-// Crea el pool de conexiones (más eficiente que una conexión simple)
-const pool = mysql.createPool(process.env.MYSQL_URL);
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
+});
 
-// Prueba la conexión al iniciar
 pool.getConnection()
-    .then(connection => {
+    .then(conn => {
         console.log("✅ Conexión a la DB de Railway exitosa!");
-        connection.release(); // Libera la conexión
+        conn.release();
     })
     .catch(err => {
         console.error("❌ Error al conectar con la DB de Railway:", err.message);
