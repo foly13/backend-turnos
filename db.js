e94078c8bb400b4fa0cb914670b827a2dbfddb94
foly13 <foly_13@hotmail.com>
2025-11-14 10:03:21 -0300

    cambio env

--- db.js
+++ db.js
@@ -4,11 +4,11 @@ import 'dotenv/config'; // Carga las variables de entorno
 
 // Crea el pool de conexiones (más eficiente que una conexión simple)
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
